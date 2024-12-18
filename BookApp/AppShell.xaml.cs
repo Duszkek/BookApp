bee477340d9b2259ba5ef1028bb6b5ac44c60061
Kamil Kukuła <duszke@Mac-miniuszek.local>
2024-12-18 14:20:30 +0100

    Initial commit

---
+++ BookApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BookApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BookApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BookApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
